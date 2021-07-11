aaa305167590c20d437a73c70af626afb0da6429
AnnaE <annaefitzgerald@gmail.com>
2021-07-11 15:02:43 -0600

    cleaned up comments

--- checkerboard.py
+++ checkerboard.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 def checkerboard_eight():
     return render_template('index.html', x = int(8))
 
-# #2. Display a 8 by 4 checkerboard
+#2. Display a 8 by 4 checkerboard
 @app.route('/4')
 def checkerboard_four():
     return render_template('index.html', x = int(8), y = int(4))
 
+#3. Generate random checkerboard
 @app.route('/<int:x>/<int:y>')
 def checkers_random(x=int(),y=int()):
     return render_template('index.html', x = x, y = y)
@@ -19,10 +20,3 @@ def checkers_random(x=int(),y=int()):
 
 if __name__=="__main__":
     app.run(debug=True)
-
-
-
-# @app.route('/<int:x>/<int:y>')
-# def checkers_random(x, y):
-#             return render_template('index.html', x = x, y = y, checkers_dark = checkers_dark, checkers_light = checkers_light, checkerboard = checkerboard)
-
